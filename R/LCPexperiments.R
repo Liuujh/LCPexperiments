eff89193350ea7a5013dba9d37d4a19620a8a670
Jiahao Liu <158493713+Liuujh@users.noreply.github.com>
2024-10-29 14:59:23 +0800

    Update LCPexperiments.R

--- R/LCPexperiments.R
+++ R/LCPexperiments.R
@@ -133,7 +133,7 @@ LCPcompare <- function(xtrain, ytrain, xcalibration, ycalibration,
   Vcv = observed_sds[[1]]
   order1 = order(Vcal)
   
-  myLCR = LCPmodule$new(H = Hlists$H[order1, order1], V = Vcal[order1], h = 2, alpha = alpha, type = "distance", weights = weights[order1])
+  myLCR = LCPmodule$new(H = Hlists$H[order1, order1], V = Vcal[order1], h = 2, alpha = alpha, type = "neighbor", weights = weights[order1])
   
   t1 = Sys.time()
   auto_ret = myLCR$LCP_auto_tune(V0 = Vcv, H0 = Hlists$Hcv, hs = hs, B = 2, delta = alpha/2, lambda = 1, trace = TRUE)
@@ -162,7 +162,7 @@ LCPcompare <- function(xtrain, ytrain, xcalibration, ycalibration,
   Vcv = observed_sds[[1]]/estimated_sds[[1]]
   order1 = order(Vcal)
   
-  myLCR = LCPmodule$new(H = Hlists$H[order1, order1], V = Vcal[order1], h = 2, alpha = alpha, type = "distance", weights = weights[order1])
+  myLCR = LCPmodule$new(H = Hlists$H[order1, order1], V = Vcal[order1], h = 2, alpha = alpha, type = "neighbor", weights = weights[order1])
   
   t1 = Sys.time()
   auto_ret = myLCR$LCP_auto_tune(V0 =Vcv, H0 =Hlists$Hcv, hs = hs, B = 2, delta=alpha/2, lambda = 1, trace = TRUE)
